f0991d0393067edbd08e34a1a21c2a159a90ef49
Mihai Farcas <farcasmihai91@gmail.com>
2016-05-18 00:51:35 +0300

    Basic Functionality
    Added the extractfile.js module.

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # dcm-document-extractor
+
+Cross-platform shell script that extracts the embedded PDF document from a DICOM file.
+
+To run use: <i> node /index.js -i 'dicom/path' -o 'pdf/path'.<i> from the script's location.
+To see more infromation use: <i> node /index.js --help<i>
--- index.js
+++ index.js
@@ -2,11 +2,27 @@
  * Written by Farcas Mihai
  */
 var fs = require('fs');
-var DicomParser = require('dicom-parser');
+var path = require('path');
 var options = require('./options');
+var extractor = require('./extractfile');
 
 // The shellscript accepts parameters
 var dcmDirectory = options.dcmDirectory
 var outDirectory = options.outDirectory
 
 console.log(`from ${dcmDirectory} to ${outDirectory}`);
+
+if (fs.existsSync(dcmDirectory)) {
+  extractor.extractEmbeddedDocument(dcmDirectory).then(
+    function (buffer) {
+      fs.writeFile(path.join(outDirectory, 'embeddedfile.pdf'), buffer, 'utf8', function (err) {
+        if(err){
+          return console.log(err);
+        }
+        console.log('success');
+      });
+    }
+  );
+} else {
+  console.log('ERROR: No file at source')
+}
